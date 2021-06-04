5a87ac1109e00033bc0d09dadddbdc5845904203
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-06-04 21:12:30 +0300

    change server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,4 +19,13 @@ export const createPosts = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
-};
+}
+export const updatePosts =async (req,res)=>{
+  const {id:_id} =req.params;
+  const post=req.body;
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id");
+  const updatePosts=await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+
+  res.json(updatePosts);
+}
