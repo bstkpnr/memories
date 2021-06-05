31cf30ad7e4b3d958d0efccc84854bd4aa769fa5
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-06-06 00:55:04 +0300

    edit posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPosts, createPosts, updatePost, deletePost } from "../controllers/posts.js";
+import { getPosts, createPosts, updatePost, deletePost,likePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.get('/', getPosts);
 router.post('/', createPosts);
 router.put('/:id', updatePost);
 router.delete('/:id',deletePost);
-router.put('/:id/likePost')
+router.put('/:id/likePost',likePost)
 
 export default router;
 
